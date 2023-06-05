4e9541d62db228c31186b3096192727ca3f37da8
Kyrylo Vitchenko <kyrylo.vitchenko@vaisala.com>
2023-06-05 17:39:19 +0300

    remove ocr

--- src/file-upload/strategies/pdf-text-extraction.strategy.ts
+++ src/file-upload/strategies/pdf-text-extraction.strategy.ts
@@ -7,18 +7,12 @@ import * as pdf2img from 'pdf-img-convert';
 export class PdfTextExtractionStrategy implements TextExtractionStrategy {
   async extractText(fileBuffer: Buffer): Promise<string> {
     try {
-      // Convert PDF to images
-      const imageBuffers = await this.convertPdfToImages(fileBuffer);
-
       // Extract text using pdf-parse
       const parsedData = await PDFParser(fileBuffer);
       const parsedText = parsedData.text;
 
-      // Perform OCR on the images
-      const ocrText = await this.performOcrOnImages(imageBuffers);
-
       // Combine parsed text and OCR text
-      const combinedText = `${parsedText}\n${ocrText}`;
+      const combinedText = `${parsedText}`;
 
       // Remove newline characters and null bytes
       const finalText = combinedText
@@ -30,33 +24,5 @@ export class PdfTextExtractionStrategy implements TextExtractionStrategy {
       console.log(err);
       throw new BadRequestException('Invalid file provided.');
     }
-  }
-
-  private async convertPdfToImages(pdfBuffer: Buffer): Promise<Buffer[]> {
-    const imageBuffers = await pdf2img.convert(pdfBuffer);
-
-    const bufferArray = imageBuffers.map((imageBuffer) =>
-      Buffer.from(imageBuffer),
-    );
-
-    return bufferArray;
-  }
-
-  private async performOcrOnImages(imageBuffers: Buffer[]): Promise<string> {
-    const worker = await Tesseract.createWorker();
-    await worker.loadLanguage('eng+rus');
-    await worker.initialize('eng+rus');
-
-    const ocrTexts = [];
-    for (const imageBuffer of imageBuffers) {
-      const {
-        data: { text },
-      } = await worker.recognize(imageBuffer);
-      ocrTexts.push(text);
-    }
-
-    await worker.terminate();
-
-    return ocrTexts.join('\n');
   }
 }
