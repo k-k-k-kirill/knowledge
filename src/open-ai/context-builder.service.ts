16748c4caecbbafb61de027cd96fbed29317c1f0
Kyrylo Vitchenko <kyrylo.vitchenko@vaisala.com>
2023-06-09 21:55:15 +0300

    token window

--- src/open-ai/context-builder.service.ts
+++ src/open-ai/context-builder.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import {
   MessagesSchema,
   MessageAuthor,
-} from 'src/messages/schema/messages.schema';
+} from '../messages/schema/messages.schema';
 import { UserMessage } from './messages/user-message';
 import { AssistantMessage } from './messages/assistant-message';
 import { TextSectionSchema } from '../text-sections/schemas/text-section.schema';
 import { SystemMessage } from './messages/system-message';
 import { PromptsService } from 'src/prompts/prompts.service';
+import { encode, decode, isWithinTokenLimit } from 'gpt-tokenizer';
 
 @Injectable()
 export class ContextBuilderService {
@@ -23,13 +24,43 @@ export class ContextBuilderService {
     });
   }
 
+  truncateContextMessages(
+    systemMessage: SystemMessage,
+    messages: any[],
+    userMessage: UserMessage,
+  ): any[] {
+    const truncatedMessages = [];
+    const tokenLimit = 3096;
+
+    const systemMessageTokenCount = encode(
+      JSON.stringify(systemMessage.toJSON()),
+    ).length;
+
+    const userMessageTokenCount = encode(
+      JSON.stringify(userMessage.toJSON()),
+    ).length;
+
+    let totalTokens = systemMessageTokenCount + userMessageTokenCount;
+
+    for (let i = messages.length - 1; i >= 0; i--) {
+      const messageTokenCount = encode(JSON.stringify(messages[i])).length;
+
+      if (totalTokens + messageTokenCount < tokenLimit) {
+        truncatedMessages.unshift(messages[i]);
+        totalTokens += messageTokenCount;
+      } else {
+        break;
+      }
+    }
+
+    return truncatedMessages;
+  }
+
   buildMessages(
     text: string,
     textSections: TextSectionSchema[],
     messages: MessagesSchema[],
   ) {
-    const contextMessages = this.getContextMessages(messages);
-
     const systemMessage = new SystemMessage(
       `${this.promptsService.getBasePrompt()} ${this.promptsService.getSourcesPrompt(
         textSections,
@@ -37,6 +68,14 @@ export class ContextBuilderService {
     );
     const userMessage = new UserMessage(text);
 
-    return [systemMessage.toJSON(), ...contextMessages, userMessage.toJSON()];
+    const contextMessages = this.getContextMessages(messages);
+
+    const truncatedContextMessages = this.truncateContextMessages(
+      systemMessage,
+      contextMessages,
+      userMessage,
+    );
+
+    return [systemMessage.toJSON(), ...truncatedContextMessages];
   }
 }
